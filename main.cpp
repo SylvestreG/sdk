cbb4d08282f310a8608b29f960979b8cae5adb1b
Sylvestre Gallon <ccna.syl@gmail.com>
2020-01-18 23:32:37 +0100

    feat(proxy): implement a first draft of the proxy code.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,41 @@
+#include <chrono>
+#include <iostream>
+#include <sstream>
 #include <served/served.hpp>
+#include <restclient-cpp/restclient.h>
 
-int main(int argc, char const* argv[]) {
+constexpr char const* base = "https://test-streams.mux.dev/x36xhzz";
+constexpr char const* root = "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8";
+
+int main(int argc, char const *argv[]) {
   // Create a multiplexer for handling requests
   served::multiplexer mux;
 
-  // GET /hello
-  mux.handle("/hello.m3u8")
+  // Global http endpoint
+  mux.handle("/{addr}")
     .get([](served::response& res, const served::request& req) {
-      res << "Hello world!";
+      std::string url;
+
+      if (req.url().URI() == "/hello.m3u8") {
+        url = root;
+      } else {
+        std::stringstream ss;
+        ss << base << req.url().URI();
+        url = std::move(ss.str());
+      }
+
+      std::cout << "[IN] " << url << std::endl;
+      std::chrono::system_clock timer;
+
+      auto t1 = timer.now();
+      RestClient::Response r = RestClient::get(url);
+      auto t2 = timer.now();
+
+      std::cout << "[OUT] " << url << " ("
+        << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count()
+        << "ms)"
+        << std::endl;
+      res << r.body;
     });
 
   // Create the server and run with 10 handler threads.
